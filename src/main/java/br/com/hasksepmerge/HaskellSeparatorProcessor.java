eafe36a7785185d0e2067bb54b64ede256d685de
sudown <mirosmarsantoss@gmail.com>
2026-03-12 10:13:18 -0300

    refact: adiconado tratamento costeticos para IDEs localizarem areas de conflito

--- src/main/java/br/com/hasksepmerge/HaskellSeparatorProcessor.java
+++ src/main/java/br/com/hasksepmerge/HaskellSeparatorProcessor.java
@@ -17,14 +17,23 @@ public class HaskellSeparatorProcessor {
     }
 
     public static String postprocessCodeBlock(String mergedCodeBlock) {
+        // 1. Limpeza do fantasma do Windows (\r)
         String result = mergedCodeBlock.replace("\r", "");
 
-        // O método replace() do Java trabalha com texto literal (não usa Regex).
-        // Ele vai varrer o arquivo e desfazer EXATAMENTE a injeção que fizemos no pré-processamento,
-        // devolvendo a formatação e os espaçamentos estruturais idênticos aos do código original.
+        // 2. A Mágica da Reversão Literal (INTOCÁVEL - Garante a Sintaxe do Haskell)
         for (String sep : SEPARATORS_LIST) {
             result = result.replace("\n" + sep + "\n", sep);
         }
+
+        // 3. PASSO COSMÉTICO: Garantir a formatação dos marcadores para as IDEs (VS Code)
+        // a) Se o marcador colou à direita de um código (ex: =<<<<<<<), empurra o marcador para a linha de baixo
+        result = result.replaceAll("([^\\n])(<<<<<<<|\\|\\|\\|\\|\\|\\|\\||=======|>>>>>>>)", "$1\n$2");
+
+        // b) Se o código colou à direita de um marcador, empurra o código para a linha de baixo.
+        // O ======= é fixo e fácil de tratar:
+        result = result.replaceAll("(?m)^(=======)([^\\n])", "$1\n$2");
+        // Os outros terminam obrigatoriamente com o nome do nosso arquivo temporário (.hs):
+        result = result.replaceAll("(?m)^(<<<<<<<.*?\\.hs|\\|\\|\\|\\|\\|\\|\\|.*?\\.hs|>>>>>>>.*?\\.hs)([^\\n])", "$1\n$2");
 
         return result;
     }
